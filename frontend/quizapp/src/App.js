f2debef5932f225f6a0753c30fa1259dc580ee53
Shiwang0-0 <bisht0shiwang@gmail.com>
2023-11-15 18:29:39 +0530

    App files with CSS with Home,Login,Register,Quiz components

---
+++ frontend/quizapp/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+// import Home from './components/home/home';
+// import Login from './components/login/login';
+import Register from './components/register/register';
+// import Quiz from './components/quiz/quiz';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Home/> */}
+       {/* <Login/> */}
+      <Register/> 
+      {/* <Quiz/> */}
+    </div>
+  );
+}
+
+export default App;
