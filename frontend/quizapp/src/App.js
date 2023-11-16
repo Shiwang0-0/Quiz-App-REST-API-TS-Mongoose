34f48b085e3ff0584497169d62f7fe4aa4eff11d
Shiwang0-0 <bisht0shiwang@gmail.com>
2023-11-16 21:32:11 +0530

    Added Create,update,get,publish component and added routes for different components

--- frontend/quizapp/src/App.js
+++ frontend/quizapp/src/App.js
@@ -1,17 +1,31 @@
 import './App.css';
-
-// import Home from './components/home/home';
-// import Login from './components/login/login';
+import {Routes,Route} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import Home from './components/home/home';
+import Login from './components/login/login';
 import Register from './components/register/register';
-// import Quiz from './components/quiz/quiz';
-
+import Quiz from './components/quiz/quiz';
+import { CreateQuiz,UpdateQuiz,GetQuiz,PublishQuiz } from './components/quiz/quiz';
+import Exam from './components/exam/exam';
 function App() {
+
   return (
     <div className="App">
-      {/* <Home/> */}
-       {/* <Login/> */}
-      <Register/> 
-      {/* <Quiz/> */}
+        <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/home' element={<Home/>}></Route>
+          <Route path='/register' element={<Register/> }></Route>
+          <Route path='/quiz' element={<Quiz/>}></Route>
+          <Route path='/createquiz' element={<CreateQuiz/>}></Route>
+          <Route path='/updatequiz' element={<UpdateQuiz/>}></Route>
+          <Route path='/getquiz' element={<GetQuiz/>}></Route>
+          <Route path='/publishquiz' element={<PublishQuiz/>}></Route>
+          <Route path='/exam' element={<Exam/>}></Route>
+        </Routes>
+        </BrowserRouter>
+        
+         
     </div>
   );
 }
