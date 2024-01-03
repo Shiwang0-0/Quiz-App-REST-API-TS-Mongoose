e9d20d4d002c93faeca909d38f25e6c115a7e879
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-01-03 14:29:43 +0530

    Alert added

--- frontend/quizapp/src/App.js
+++ frontend/quizapp/src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
 import {Routes,Route} from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom';
-import Home from './components/home/home';
-import Login from './components/login/login';
-import Register from './components/register/register';
-import Quiz from './components/quiz/quiz';
-import { CreateQuiz,UpdateQuiz,GetQuiz,PublishQuiz } from './components/quiz/quiz';
-import Exam from './components/exam/exam';
+import Home from './components/home/Home';
+import Login from './components/login/Login';
+import Register from './components/register/Register';
+import Quiz from './components/quiz/Quiz';
+import { CreateQuiz,UpdateQuiz,GetQuiz,PublishQuiz } from './components/quiz/Quiz';
+import Exam from './components/exam/Exam';
+import Alert from './components/Alert/Alert';
 function App() {
 
   return (
     <div className="App">
+      <Alert message={"this is alert"}/>
         <BrowserRouter>
         <Routes>
-          <Route path='/login' element={<Login/>}></Route>
+        
+          <Route path='/' element={<Login/>}></Route>
           <Route path='/home' element={<Home/>}></Route>
           <Route path='/register' element={<Register/> }></Route>
           <Route path='/quiz' element={<Quiz/>}></Route>
@@ -22,6 +25,7 @@ function App() {
           <Route path='/getquiz' element={<GetQuiz/>}></Route>
           <Route path='/publishquiz' element={<PublishQuiz/>}></Route>
           <Route path='/exam' element={<Exam/>}></Route>
+         
         </Routes>
         </BrowserRouter>
         
