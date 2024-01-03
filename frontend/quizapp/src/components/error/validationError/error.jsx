511f3ec40290057741705bf1e1bc024f4183e58c
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-01-03 14:27:26 +0530

    Error component

---
+++ frontend/quizapp/src/components/error/validationError/error.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./error.css";
+
+export const getResponseError=(error)=>{
+    if((error===null)||(error===undefined))
+    {
+        return null;
+    }
+    if(error.response)
+    {
+        if(error.response.status===422 && error.response.data)
+        {
+           const responseError=error.response.data;
+           if(responseError)
+           {
+            const errorData={};
+           
+           for(const errorItem of responseError)
+           {
+            errorData[errorItem.field]=errorItem.msg;
+           }
+           return errorData;
+        }
+        
+        }
+        return null;
+    }
+}
+
+export const Error=(error)=>{
+    return(
+        <div className="errorMsg">{error.message}</div>
+    );
+}
+
+export default getResponseError
