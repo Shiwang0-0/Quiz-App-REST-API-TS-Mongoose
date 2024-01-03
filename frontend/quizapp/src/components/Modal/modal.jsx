dd66d9447845b0af904c0515bcc76fbcfbc9f6c6
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-01-03 14:30:27 +0530

    Validation in Modal and style changes

--- frontend/quizapp/src/components/Modal/modal.jsx
+++ frontend/quizapp/src/components/Modal/modal.jsx
@@ -1,14 +1,48 @@
 import  "./modal.css"
-import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const OtpModal = ({closeModal}) => {
+
+  const sendOTP=async (e)=>{
+    e.preventDefault();
+    const response = await fetch('http://localhost:3002/auth/send-otp', {
+      method: "POST",
+      body: JSON.stringify(form),
+      headers: {
+        'Content-Type':'application/json'
+      },
+    })
+    const data = await response.text();
+    console.log(data)
+    closeModal();
+  }
+
+  const [form, setForm] = useState({
+    email: ""
+  });
+  const handleForm = (e) => {
+    console.log(e.target.value,e.target.name);
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
     return (
       <>
       <div className="modalWrapper" onClick={closeModal}></div>
         <div className="modalContainer">
-            <p>Enter Otp recieved on your mail</p>
-            <input></input>
-            <button onClick={closeModal} ><Link to="/home" style={{ textDecoration: 'none',color:'inherit' }}> Done </Link></button>
+            <p>Enter the email to get the OTP</p>
+            <input 
+            
+              type="email"
+              placeholder="Enter Your Email"
+              name="email"
+              onChange={handleForm}
+              value={setForm.email}
+              required>
+            </input>
+            <button onClick={sendOTP} >Done </button>
+
         </div>
       </>
     );
